a5d592092aadbbdeaef074b2298b74d4e89d52d5
eldarm <junk@eldar.com>
2023-09-01 13:52:54 +0200

    Final clean up, nothing functionally iportant.

--- b.sh
+++ b.sh
@@ -1,4 +1,10 @@
-echo "Compiling scrape GOARCH=${GOARCH} GOOS=${GOOS}..."
-go build scrape/*
-echo "Compiling main..."
-go build main/scraper.go
+echo "Compiling main for Mac..."
+GOARCH="arm64" GOOS="darwin" go build main/scraper.go
+echo "Compiling main for linux..."
+GOARCH="amd64" GOOS="linux" go build -o scraperlx main/scraper.go
+echo "Compiling main for windows..."
+GOARCH="amd64" GOOS="windows" go build main/scraper.go
+echo "Publishing..."
+cp scraper bin/scraper.app
+cp scraperlx bin/
+cp scraper.exe bin/
--- wb.sh
+++
@@ -1,7 +0,0 @@
-GOARCH="amd64"
-GOOS="windows"
-echo "Compiling scrape GOARCH=${GOARCH} GOOS=${GOOS}..."
-go build scrape/*
-echo "Compiling main..."
-go build main/scraper.go
-# go -o bin/scraper.exe build main/scraper.go
